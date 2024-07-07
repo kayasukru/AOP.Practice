a11fa9086e3af5a61ed6e9846641645fc9ce6c2e
Sukru Kaya <kayasukru@gmail.com>
2024-07-07 14:18:46 +0300

    Add project files.

---
+++ 1-ProxyApp/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var b1 = new BusinessModule();
+        b1.Method();
+        Console.WriteLine("------------------------");
+
+        var b2 = new BusinessModuleProxy();
+        b2.Method();
+    }
+}
+
+public interface IBusinessModule
+{
+    void Method();
+}
+
+public class BusinessModule : IBusinessModule
+{
+    public void Method()
+    {
+        Console.WriteLine("Method ...");
+    }
+}
+
+public class BusinessModuleProxy : IBusinessModule
+{
+    private BusinessModule _realObject;
+
+    public BusinessModuleProxy()
+    {
+        _realObject = new BusinessModule();
+    }
+
+    public void Method()
+    {
+        Console.WriteLine("Before method.");
+        _realObject.Method();
+        Console.WriteLine("After method.");
+    }
+}
